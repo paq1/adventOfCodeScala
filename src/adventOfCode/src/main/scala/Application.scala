d09d3313da39cc471206a341c3b38e403c6bb7ef
Pierre Paquin <p.paquin1016@laposte.net>
2023-12-03 20:16:43 +0100

    refactor: clean project

--- src/adventOfCode/src/main/scala/Application.scala
+++ src/adventOfCode/src/main/scala/Application.scala
@@ -1,8 +1,8 @@
 import days.year2023.day4.{Day4Part1, Day4Part2}
-import services.InputReaderFile
+import services.InputReaderFromFile
 
 object Application extends App {
-  private val inputReader = new InputReaderFile("data/2023/day4")
+  private val inputReader = new InputReaderFromFile("data/2023/day4")
   private val part1 = new Day4Part1(inputReader).result
   private val part2 = new Day4Part2(inputReader).result
 
--- src/adventOfCode/src/main/scala/services/InputReaderFromFile.scala
+++ src/adventOfCode/src/main/scala/services/InputReaderFromFile.scala
@@ -2,7 +2,7 @@ package services
 
 import scala.io.Source
 
-class InputReaderFile(filePath: String) extends InputReader {
+class InputReaderFromFile(filePath: String) extends InputReader {
   override def getList: List[String] = {
     val source = Source.fromFile(filePath)
     val lines = source.getLines.toList
